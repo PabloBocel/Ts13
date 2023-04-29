65be594140d74167e41f67740ae6091449bf97f6
Pablo <andresbocel0@gmail.com>
2023-04-29 16:33:44 -0600

    Primera parte vacia

---
+++ Ts13git-PABM-1109623/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ts13git_PABM_1109623
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
