22fdd708ac6bcc60a77abe71109454a57ea48330
Pablo <andresbocel0@gmail.com>
2023-04-29 16:38:49 -0600

    Primer ejercicio

--- Ts13git-PABM-1109623/Program.cs
+++ Ts13git-PABM-1109623/Program.cs
@@ -6,10 +6,60 @@ using System.Threading.Tasks;
 
 namespace Ts13git_PABM_1109623
 {
-    internal class Program
+    internal class Persona
     {
+        private string Nombre;
+        private string Apellido;
+        private int Edad;
+        private double Altura;
+        public Persona()
+        {
+
+
+            Console.WriteLine("Ingrese un Nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese un Apellido");
+            Apellido = Console.ReadLine();
+            Console.WriteLine("Ingrese Edad");
+            Edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese Altura");
+            Altura = Convert.ToDouble(Console.ReadLine());
+
+        }
+        public void NombreCompleto()
+        {
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Apellido: " + Apellido);
+            Console.WriteLine("Edad: " + Edad + "años");
+            Console.WriteLine("Altura: " + Altura + "cm");
+        }
+
+
         static void Main(string[] args)
         {
+
+            string menu = "";
+            while (!menu.Equals("3"))
+            {
+                Console.WriteLine("Trabajo supervisado semana 13_PABM_1109623");
+                Console.WriteLine("Ingrese una opción del menú");
+                Console.WriteLine("1. Altura de una persona" + "\n2. Tabla de multiplicación" + "\n3. Salir");
+                int mum = Convert.ToInt32(Console.ReadLine());
+                switch (mum)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Trabajo supervisado semana 13_PABM_1109623");
+                        Console.WriteLine("Altura de una persona\n");
+                        Persona met = new Persona();
+                        met.NombreCompleto();
+
+
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
         }
     }
 }
